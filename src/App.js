2c0454527d23703019a63241c5ce35fffe6f7f5c
AndersonDev22 <luizandersonss222@gmail.com>
2022-05-19 01:04:28 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+//USE CALLBACK GERALMENTE USANDO EM OTIMIZAÇÕES
+//USAR EM FUNÇÕES QUE RENDERIZEM COISAS PESADAS
+
+//useCallback(função, [array de dependências])
+
+import P from 'prop-types';
 import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+
+const Button = React.memo(function Button({ incrementButton }) {
+  return <button onClick={() => incrementButton(10)}>+</button>
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num); //função de callback
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>useCallback</p>
+      <h1>Contador: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
-
 export default App;
+
